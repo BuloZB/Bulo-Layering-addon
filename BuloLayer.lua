b33eb09ba3b401b0445c0753824387bc6398ef4c
Bulo <BuloZB@users.noreply.github.com>
2020-06-20 04:45:06 -0700

    update detection changes

--- BuloLayer.lua
+++ BuloLayer.lua
@@ -1,7 +1,9 @@
 BuloLayer = LibStub("AceAddon-3.0"):NewAddon("BuloLayer", "AceConsole-3.0", "AceEvent-3.0", "AceComm-3.0", "AceTimer-3.0")
 BuloLayer.Dialog = LibStub("AceConfigDialog-3.0")
 BuloLayer:RegisterChatCommand("lh", "ChatCommand")
-BuloLayer.VERSION = 151
+BuloLayer.VERSION = 156
+
+local L = LibStub("AceLocale-3.0"):GetLocale("BuloLayer")
 
 function GetVersionString(ver)
 	if ver >= 10 then
@@ -12,29 +14,28 @@ function GetVersionString(ver)
 end
 
 BuloLayer.options = {
-	name = "|TInterface\\AddOns\\BuloLayer\\Media\\swap:24:24:0:5|t BuloLayer " .. GetVersionString(BuloLayer.VERSION),
+	name = "|TInterface\\AddOns\\BuloLayer\\Media\\swap:24:24:0:5|t " .. L["Layer Hopper"] .. " " .. GetVersionString(BuloLayer.VERSION),
 	handler = BuloLayer,
 	type = 'group',
 	args = {
 		desc = {
 			type = "description",
-			name = "|CffDEDE42Layer Hopper Config (You can type /lh config to open this).\n"
-					.. "Auto inviting will be disabled automatically if inside an instance or battleground and when in a battleground queue.\n",
+			name = "|CffDEDE42" .. L["optionsDesc"],
 			fontSize = "medium",
 			order = 1,
 		},
 		autoinvite = {
 			type = "toggle",
-			name = "Auto Invite",
-			desc = "Enable auto invites for layer switch requests in the guild (if you turn this off you cannot be used by other guildies to switch layers).",
+			name = L["Auto Invite"],
+			desc = L["autoInviteDesc"],
 			order = 2,
 			get = "getAutoInvite",
 			set = "setAutoInvite",
 		},
 		minimap = {
 			type = "toggle",
-			name = "Minimap Button",
-			desc = "Enable minimap button (allows for quick layer hop requests and shows current layer).\nWill require a /reload if hiding the button.",
+			name = L["Minimap Button"],
+			desc = L["minimapDesc"],
 			order = 3,
 			get = "getMinimap",
 			set = "setMinimap",
@@ -77,7 +78,7 @@ BuloLayer.SendLayerMinMaxPrefix = "LH_slmm"
 BuloLayer.SendLayerMinMaxWhisperPrefix = "LH_slmmw"
 BuloLayer.SendResetLayerDataPrefix = "LH_srld"
 BuloLayer.DEFAULT_PREFIX = "BuloLayer"
-BuloLayer.CHAT_PREFIX = "|cFFFF69B4[BuloLayer]|r "
+BuloLayer.CHAT_PREFIX = format("|cFFFF69B4[%s]|r ", L["BuloLayer"])
 BuloLayer.minLayerId = -1
 BuloLayer.maxLayerId = -1
 BuloLayer.currentLayerId = -1
@@ -89,7 +90,7 @@ BuloLayer.layerIdRange = 100 -- this is a guess based on anecdotal data of max l
 function BuloLayer:OnInitialize()
 	self.BuloLayerLauncher = LibStub("LibDataBroker-1.1"):NewDataObject("BuloLayer", {
 		type = "launcher",
-		text = "BuloLayer",
+		text = L["Layer Hopper"],
 		icon = "Interface/AddOns/BuloLayer/Media/swap",
 		OnClick = function(self, button)
 			if button == "LeftButton" then
@@ -101,25 +102,29 @@ function BuloLayer:OnInitialize()
 		OnEnter = function(self)
 			local layerText = ""
 			if BuloLayer.paused then
-				layerText = "Resetting layer data for the guild. Should only take a few more seconds..."
+				layerText = L["paused"]
 			elseif BuloLayer.currentLayerId < 0 then
-				layerText = "Unknown Layer. Target any NPC or mob to get current layer.\n(layer id: " .. BuloLayer.currentLayerId .. ", min: " .. BuloLayer.minLayerId .. ", max: " .. BuloLayer.maxLayerId .. " )"
+				layerText = format(L["unknownLayer"],
+						BuloLayer.currentLayerId, BuloLayer.minLayerId, BuloLayer.maxLayerId)
 			elseif not BuloLayer:MinMaxValid(BuloLayer.minLayerId, BuloLayer.maxLayerId) then
 				if BuloLayer.minLayerId < 0 or BuloLayer.maxLayerId < 0 then
-					layerText = "Min/max layer IDs are unknown.\n"
+					layerText = L["minMaxUnknown"]
 				else
-					layerText = "Min/max layer ID range is not large enough.\n"
+					layerText = L["rangeTooSmall"]
 				end
-				layerText = layerText .. "Need more data from guild to determine current layer.\n (layer id: " .. BuloLayer.currentLayerId .. ", min: " .. BuloLayer.minLayerId .. ", max: " .. BuloLayer.maxLayerId .. " )"
+				layerText = layerText .. "\n" ..
+						format(L["needMoreData"], BuloLayer.currentLayerId, BuloLayer.minLayerId, BuloLayer.maxLayerId)
 			else
-				layerText = "Current Layer: " .. BuloLayer:GetLayerGuess(BuloLayer.currentLayerId, BuloLayer.minLayerId, BuloLayer.maxLayerId) .. "\n(layer id: " .. BuloLayer.currentLayerId .. ", min: " .. BuloLayer.minLayerId .. ", max: " .. BuloLayer.maxLayerId .. " )"
+				layerText = format(L["currentLayer"],
+						BuloLayer:GetLayerGuess(BuloLayer.currentLayerId, BuloLayer.minLayerId, BuloLayer.maxLayerId),
+						BuloLayer.currentLayerId, BuloLayer.minLayerId, BuloLayer.maxLayerId)
 			end
 			GameTooltip:SetOwner(self, "ANCHOR_LEFT")
-			GameTooltip:AddLine("|cFFFFFFFFLayer Hopper|r " .. GetVersionString(BuloLayer.VERSION))
+			GameTooltip:AddLine(format("|cFFFFFFFF%s|r %s", L["Layer Hopper"], GetVersionString(BuloLayer.VERSION)))
 			GameTooltip:AddLine(layerText)
-			GameTooltip:AddLine("Left click to request a layer hop.")
-			GameTooltip:AddLine("Right click to access Layer Hopper settings.")
-			GameTooltip:AddLine("/lh to see other options")
+			GameTooltip:AddLine(L["minimapLeftClickAction"])
+			GameTooltip:AddLine(L["minimapRightClickAction"])
+			GameTooltip:AddLine(L["minimapOtherOptions"])
 			GameTooltip:Show()
 		end,
 		OnLeave = function(self)
@@ -129,7 +134,7 @@ function BuloLayer:OnInitialize()
 
 	self.db = LibStub("AceDB-3.0"):New("BuloLayerOptions", BuloLayer.optionDefaults, "Default");
 	LibStub("AceConfig-3.0"):RegisterOptionsTable("BuloLayer", BuloLayer.options);
-	self.BuloLayerOptions = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("BuloLayer", "BuloLayer");
+	self.BuloLayerOptions = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("BuloLayer", L["Layer Hopper"]);
 
 	self.icon = LibStub("LibDBIcon-1.0")
 	self.icon:Register("BuloLayer", self.BuloLayerLauncher, self.db.global)
@@ -171,23 +176,23 @@ end
 
 function BuloLayer:RequestLayerHop()
 	if not IsInGuild() then
-		print(self.CHAT_PREFIX .. "Layer Hopper only works when you have joined a guild.")
+		print(self.CHAT_PREFIX .. L["noGuildErr"])
 		return
 	elseif IsInGroup() then
-		print(self.CHAT_PREFIX .. "Can't request layer hop while in a group.")
+		print(self.CHAT_PREFIX .. L["inGroupErr"])
 		return
 	elseif self.currentLayerId < 0 then
-		print(self.CHAT_PREFIX .. "Can't request layer hop until your layer is known. Target any NPC or mob to get current layer.")
+		print(self.CHAT_PREFIX .. L["unknownLayerErr"])
 		return
 	elseif IsInInstance() then
-		print(self.CHAT_PREFIX .. "Can't request layer hop while in an instance or battleground.")
+		print(self.CHAT_PREFIX .. L["inInstanceErr"])
 		return
 	elseif self.paused then
-		print(self.CHAT_PREFIX .. "Resetting layer data for the guild. Should only take a few more seconds...")
+		print(self.CHAT_PREFIX .. L["paused"])
 		return
 	end
 	self:SendMessage(BuloLayer.RequestLayerSwitchPrefix .. "," .. self.VERSION .. "," .. self.currentLayerId .. "," .. self.minLayerId .. "," .. self.maxLayerId, "GUILD")
-	print(self.CHAT_PREFIX .. "Requesting layer hop from layer " .. self:GetLayerGuess(self.currentLayerId, self.minLayerId, self.maxLayerId) .. " to another layer.")
+	print(self.CHAT_PREFIX .. format(L["requestingHop"], self:GetLayerGuess(self.currentLayerId, self.minLayerId, self.maxLayerId)))
 end
 
 function BuloLayer:RequestAllPlayersLayers()
@@ -204,12 +209,12 @@ function BuloLayer:ResetLayerData()
 		self.minLayerId = -1
 		self.maxLayerId = -1
 		self.paused = true
-		print(self.CHAT_PREFIX .. "Resetting layer data in the guild...")
+		print(self.CHAT_PREFIX .. L["resettingLayerData"])
 		self:SendMessage(BuloLayer.SendResetLayerDataPrefix .. "," .. self.VERSION .. ",-1,-1,-1", "GUILD")
 		self:ScheduleTimer("UnPause", 3 + random() * 3)
 		self:UpdateIcon()
 	else
-		print(self.CHAT_PREFIX .. "Can't request layer data reset unless you are class lead or higher rank.")
+		print(self.CHAT_PREFIX .. L["rankTooLow"])
 	end
 end
 
@@ -222,7 +227,7 @@ function BuloLayer:OnCommReceived(prefix, msg, distribution, sender)
 		maxLayerId = tonumber(maxLayerId)
 		if ver ~= self.VERSION then
 			if ver > self.VERSION and not self.foundOldVersion then
-				print(self.CHAT_PREFIX .. "You are running an old version of Layer Hopper, please update from curseforge!")
+				print(self.CHAT_PREFIX .. L["oldVersionErr"])
 				self.foundOldVersion = true
 			end
 			if floor(ver / 10) ~= floor(self.VERSION / 10) then
@@ -275,7 +280,7 @@ function BuloLayer:OnCommReceived(prefix, msg, distribution, sender)
 						self.minLayerId = -1
 						self.maxLayerId = -1
 						self.paused = true
-						print(self.CHAT_PREFIX .. sender .. " requested a reset of layer data for the guild.")
+						print(self.CHAT_PREFIX .. format(L["playerRequestedLayerReset"], sender))
 						self:ScheduleTimer("UnPause", 3 + random() * 3)
 						self:UpdateIcon()
 						return
@@ -302,13 +307,13 @@ function BuloLayer:PrintPlayerLayerWithVersion(layerId, ver, sender)
 	end
 	local layerString = ""
 	if layerGuess < 0 then
-		layerString = "layer unknown"
+		layerString = L["layer unknown"]
 	elseif myLayerGuess > 0 and layerGuess > 0 and myLayerGuess ~= layerGuess then
-		layerString = "|cFF00A86Blayer " .. tostring(layerGuess) .. "|r"
+		layerString = "|cFF00A86B" .. format(L["layer %s"], tostring(layerGuess)) .. "|r"
 	else
-		layerString = "layer " .. tostring(layerGuess)
+		layerString = format(L["layer %s"], tostring(layerGuess))
 	end
-	print(self.CHAT_PREFIX .. sender .. ": " .. layerString .. " - " .. versionString .. " layer id: " .. layerId)
+	print(self.CHAT_PREFIX .. format(L["printPlayerLayer"], sender, layerString, versionString, layerId))
 end
 
 function BuloLayer:SendCurrentMinMax()
@@ -336,19 +341,22 @@ function BuloLayer:ChatCommand(input)
 	elseif input == "mmb" then
 		local minimap = not self:getMinimap()
 		self:setMinimap(nil, minimap)
-		local printString = self.CHAT_PREFIX .. "Minimap button "
 		if minimap then
-			printString = printString .. "shown."
+			print(self.CHAT_PREFIX .. L["minimapShown"])
 		else
-			printString = printString .. "hidden. (you will need to type /reload to show changes)"
+			print(self.CHAT_PREFIX .. L["minimapHidden"])
 		end
-		print(printString)
 	else
-		print("/lh config - Open/close configuration window\n" ..
-				"/lh hop - Request a layer hop\n" ..
-				"/lh list - List layers and versions for all guildies\n" ..
-				"/lh reset - Reset layer data for all guildies. (can only be done by class lead rank or above)\n" ..
-				"/lh mmb - Toggle minimap button.")
+		print(format("/lh config - %s\n" ..
+				"/lh hop - %s\n" ..
+				"/lh list - %s\n" ..
+				"/lh reset - %s\n" ..
+				"/lh mmb - %s",
+				L["configConsole"],
+				L["layerHopConsole"],
+				L["listLayersConsole"],
+				L["resetLayersConsole"],
+				L["toggleMinimapConsole"]))
 	end
 end
 
@@ -360,6 +368,23 @@ function BuloLayer:ToggleConfigWindow()
 	end
 end
 
+local blacklistedNpcIds = {
+	"2671",  -- Mechanical Squirrel
+	"14444", -- Orcish Orphan
+	"14878", -- Jubling
+	"15429", -- Disgusting Oozeling
+	"15706", -- Winter Reindeer
+}
+
+function BuloLayer:IsBlacklistedNpcId(npc_id)
+	for _, blacklistedId in pairs(blacklistedNpcIds) do
+		if npc_id == blacklistedId then
+			return true
+		end
+	end
+	return false
+end
+
 function BuloLayer:UpdateLayerFromUnit(unit)
 	if IsInInstance() or self.paused then
 		return
@@ -368,18 +393,18 @@ function BuloLayer:UpdateLayerFromUnit(unit)
 	local unitName, _ = UnitName(unit)
 	if guid ~= nil then
 		local unittype, zero, server_id, instance_id, zone_uid, npc_id, spawn_uid = strsplit("-", guid);
-		if UnitExists(unit) and not UnitIsPlayer(unit) and unittype ~= "Pet" and UnitLevel(unit) ~= 1 then
+		if UnitExists(unit) and not UnitIsPlayer(unit) and unittype ~= "Pet" and UnitLevel(unit) ~= 1 and not self:IsBlacklistedNpcId(npc_id) then
 			local layerId = -1
-			local _,_,_,_,i = strsplit("-", guid)
-			if i then
-				layerId = tonumber(i)
+			if zone_uid then
+				layerId = tonumber(zone_uid)
 			end
 			if layerId >= 0 and not self:IsLayerIdValid(layerId) then
-				print(self.CHAT_PREFIX .. "|cFFC21807YOU HAVE ENCOUNTERED A MOB THAT BREAKS LAYER HOPPER!|r")
-				print(self.CHAT_PREFIX .. "|cFFC21807MOB NAME: " .. tostring(unitName) .. "|r")
-				print(self.CHAT_PREFIX .. "|cFFC21807MOB GUID: " .. tostring(guid) .. "|r")
-				print(self.CHAT_PREFIX .. "|cFFC21807ZONE: " .. tostring(GetSubZoneText()) .. ", " .. tostring(GetZoneText()) .. "|r")
-				print(self.CHAT_PREFIX .. "|cFFC21807PLEASE SEND THIS INFORMATION TO KUTANO (OR REPORT ON GITHUB)!|r")
+				local errColorMsg = "|cFFC21807%s|r"
+				print(self.CHAT_PREFIX .. format(errColorMsg, L["mobErrTitle"]))
+				print(self.CHAT_PREFIX .. format(errColorMsg, format(L["mobErrName"], tostring(unitName))))
+				print(self.CHAT_PREFIX .. format(errColorMsg, format(L["mobErrGUID"], tostring(guid))))
+				print(self.CHAT_PREFIX .. format(errColorMsg, format(L["mobErrZone"], tostring(GetSubZoneText()) .. ", " .. tostring(GetZoneText()))))
+				print(self.CHAT_PREFIX .. format(errColorMsg, L["mobErrGithub"]))
 			end
 			if self:IsLayerIdValid(layerId) then
 				self.currentLayerId = layerId
